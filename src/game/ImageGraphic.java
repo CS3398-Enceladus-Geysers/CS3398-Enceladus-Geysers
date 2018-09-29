c788026d85855973498cf812cd0779af5e508a49
Peter Wesley Hutcheson <hutcheson8@gmail.com>
2018-09-28 22:35:45 -0500

    Simple Movement And Image
    I made an image appear and move on the screen according to arrow key input as a starting point.

--- src/game/ImageGraphic.java
+++ src/game/ImageGraphic.java
@@ -7,18 +7,36 @@ import java.io.FileInputStream;
 
 import javax.imageio.ImageIO;
 
+/**
+ * A graphic which is represented by a still image, as opposed to an animation.
+ */
 public class ImageGraphic extends Graphic {
 	private static final long serialVersionUID = 7604033494188278910L;
-	private static Integer sizeFactor;
 	Image image;
 
-	public ImageGraphic(Integer sizeFactor, String fileName) throws Exception {
-		ImageGraphic.sizeFactor = sizeFactor;
-		Image i = ImageIO.read(new BufferedInputStream(new FileInputStream(fileName)));
+	/**
+	 * This constructor loads and scales an image, and also sets its size.
+	 * 
+	 * @param fileName File name for image to be loaded.
+	 * @param width    Width of this graphic, before scaling by
+	 *                 {@link Main#SIZE_FACTOR}
+	 * @param height   Height of this graphic, before scaling by
+	 *                 {@link Main#SIZE_FACTOR}
+	 * @throws Exception In case a file is missing.
+	 */
+	public ImageGraphic(String fileName, double width, double height) throws Exception {
+		setSize((int) (width * Main.SIZE_FACTOR), (int) (height * Main.SIZE_FACTOR));
+		image = ImageIO.read(new BufferedInputStream(new FileInputStream(fileName))).getScaledInstance(getWidth(),
+				getHeight(), Image.SCALE_DEFAULT);
 	}
 
+	/**
+	 * Overrides paintComponent to paint the still image.
+	 * 
+	 * @see game.Graphic#paintComponent(java.awt.Graphics)
+	 */
 	@Override
-	public void paint(Graphics g) {
+	public void paintComponent(Graphics g) {
 		g.drawImage(image, 0, 0, null);
 	}
 }
--- src/game/Main.java
+++ src/game/Main.java
@@ -1,63 +1,83 @@
 package game;
 
+import java.awt.Component;
 import java.awt.Dimension;
-import java.util.ArrayList;
+import java.awt.Point;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.EnumMap;
+import java.util.LinkedList;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
-public class Main {
-	private final class GameWindow extends JFrame {
-		ArrayList<Graphic> graphics = new ArrayList<Graphic>();
-
-		public GameWindow(String string) {
-			super(string);
-		}
-
-		public void addGameComponent(GameComponent gc) {
-			// TODO Finish this method.
-		}
-	}
-
+/**
+ * The main driver class for the game, also a {@link KeyListener}.
+ */
+public class Main implements KeyListener {
 	/**
 	 * This class represents a scene which we can swap to using
 	 * transitionScene(Scenes)
 	 */
 	class Scene extends JPanel {
 		private static final long serialVersionUID = -6926746677172868739L;
-		private final Dimension size;
+		private final Point cameraLocation;
+		private final LinkedList<GameComponent> gameComponents = new LinkedList<GameComponent>();
 
-		public Scene(Dimension size) {
-			this.size = size;
+		public Scene() {
+			cameraLocation = new Point(0, 0);
+			setLayout(null);
+		}
+
+		/** Will be called on every frame. */
+		public void act() {
+			for (GameComponent gc : gameComponents) {
+				if (gc.needsPainting()) {
+					gc.markPainted();
+					for (Graphic g : gc.getGraphics()) {
+						boolean flag = true;
+						Component[] c = getComponents();
+						for (int x = 0; x < c.length; x++) {
+							if (c[x] == g) {
+								flag = false;
+							}
+						}
+						if (flag)
+							add(g);
+					}
+				}
+				gc.updateBounds();
+			}
+			Component[] c = getComponents();
+			for (int x = 0; x < c.length; x++) {
+				if (((Graphic) c[x]).isExpired())
+					remove(c[x]);
+			}
+		}
+
+		public void addGameComponent(GameComponent gc) {
+			gameComponents.add(gc);
+			gc.repaint();
 		}
 
 		@Override
 		public final Dimension getPreferredSize() {
-			return size;
+			return GAME_PANEL_DIMENSION;
 		}
-	};
+	}
 
 	/** A list of all the scenes. */
 	enum Scenes {
 		LEVEL, MAIN_MENU, OVERWORLD, SETTINGS, TITLE
-	}
-
-	/** This is the panel which will always be displayed in the window. */
-	private static final JPanel GAME_PANEL = new JPanel() {
-		private static final long serialVersionUID = -2993152027099833672L;
-
-		@Override
-		public Dimension getPreferredSize() {
-			return GAME_PANEL_DIMENSION;
-		}
 	};
+
 	/** This determines how big the game is. */
-	private static final Integer SIZE_FACTOR = 60;
+	public static final Integer SIZE_FACTOR = 60;
+
 	/** This is the dimensions for the panel which is always displayed. */
 	private static final Dimension GAME_PANEL_DIMENSION = new Dimension(16 * SIZE_FACTOR, 9 * SIZE_FACTOR);
+	private static final JFrame GAME_WINDOW = new JFrame("Title TBA");
 	/** This variable tells us which scene we're currently in. */
 	private static Scenes scene;
 	/**
@@ -66,37 +86,80 @@ public class Main {
 	 */
 	private static final EnumMap<Scenes, Scene> SCENES = new EnumMap<Scenes, Scene>(Scenes.class);
 
-	// TODO Add a variable here that will store where the game camera is.
-	/** Here's the meat of the program. */
-	public Main() {
+	/**
+	 * Here's the meat of the program.
+	 * 
+	 * @throws Exception if a file cannot be loaded.
+	 */
+	public Main() throws Exception {
 		for (Scenes s : Scenes.values()) {
-			SCENES.put(s, new Scene(GAME_PANEL_DIMENSION));
+			SCENES.put(s, new Scene());
 		}
 		// TODO Add components to each other scene.
 		Scene level = SCENES.get(Scenes.LEVEL);// Get other scenes similarly.
-		level.add(new JLabel("Hello World!"));// Demonstration.
+		LevelComponent gc = new LevelComponent(level.cameraLocation, new Point(0, 0));
+		gc.addGraphic(new ImageGraphic("assets/Scribble.png", 1 / .6, 1 / .6));
+		level.addGameComponent(gc);
 		// TODO Add components to the level.
-		GameWindow gameWindow = new GameWindow("Title TBA");
 		transitionScene(Scenes.LEVEL);// Demonstration.
-		gameWindow.add(GAME_PANEL);
-		gameWindow.pack();
-		gameWindow.setResizable(false);
-		gameWindow.setLocationRelativeTo(null);
-		gameWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		gameWindow.setVisible(true);
+		GAME_WINDOW.addKeyListener(this);
+		GAME_WINDOW.pack();
+		GAME_WINDOW.setResizable(false);
+		GAME_WINDOW.setLocationRelativeTo(null);
+		GAME_WINDOW.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		GAME_WINDOW.setVisible(true);
+		while (true) {
+			level.act();
+		}
+	}
+
+	/** We can use this method to listen for keyboard input from our window. */
+	@Override
+	public void keyPressed(KeyEvent e) {
+		Point camera = SCENES.get(Scenes.LEVEL).cameraLocation;
+		switch (e.getKeyCode()) {
+		case 37:// Left
+			camera.translate(5, 0);
+			break;
+		case 38:// Up
+			camera.translate(0, 5);// Y+ axis is down.
+			break;
+		case 39:// Right
+			camera.translate(-5, 0);
+			break;
+		case 40:// Down
+			camera.translate(0, -5);
+			break;
+		default:
+		}
+	}
+
+	/** Let's not use this one, it still must be implemented though. */
+	@Override
+	public void keyReleased(KeyEvent e) {
+	}
+
+	/** Let's not use this one, it still must be implemented though. */
+	@Override
+	public void keyTyped(KeyEvent e) {
 	}
 
 	/** Swap out scenes to the scene specified in the parameter. */
 	private void transitionScene(Scenes scene) {
 		if (Main.scene != null)
-			GAME_PANEL.remove(SCENES.get(Main.scene));
+			GAME_WINDOW.remove(SCENES.get(Main.scene));
 		Main.scene = scene;
-		GAME_PANEL.add(SCENES.get(scene));
-		GAME_PANEL.repaint();
+		GAME_WINDOW.add(SCENES.get(scene));
+		GAME_WINDOW.repaint();
 	}
 
-	/** Just instantiate a Main Object here. */
-	public static void main(String[] args) {
+	/**
+	 * Just instantiate a Main Object here.
+	 * 
+	 * @param args arguments passed by the command line, which are ignored.
+	 * @throws Exception if a file cannot be loaded.
+	 */
+	public static void main(String[] args) throws Exception {
 		new Main();
 	}
 }
