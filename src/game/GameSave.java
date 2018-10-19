9eb62e74f5110a710c1f51c0760ad77e63468d84
lauramrice <42754734+lauramrice@users.noreply.github.com>
2018-10-18 19:40:16 -0500

    Update src/game/GameSave.java
    create GameSave class

---
+++ src/game/GameSave.java
@@ -0,0 +1,38 @@
+package game;
+
+import java.io.Serializable;
+
+public class GameSave implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int level;
+	//private Items items;
+	private int HP;
+	/*
+	try{  
+		//what type of file to save as?
+		//FileOutputStream saveGame = new FileOutputStream("SaveGame.");
+
+		//create an ObjectOutputStream to put objects into save file.
+		/*ObjectOutputStream save = new ObjectOutputStream(saveGame);
+
+		save.writeObject(level);
+		//save.writeObject(items);
+		save.writeObject(HP);
+	
+		// Close the file.
+		save.close();
+		}
+		catch(Exception exc){
+			//exc.printStackTrace(); // If there was an error, print the info.
+		}*/
+	
+}
+
+
+	
+	
+
