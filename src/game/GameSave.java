ea90a83a57d14e0712291853801ddcf5e74a75d5
lauramrice <42754734+lauramrice@users.noreply.github.com>
2018-10-27 14:21:48 -0500

    GameSave
    Added GameState and ResourceManager

--- src/game/GameSave.java
+++ src/game/GameSave.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 public class GameSave implements Serializable {
@@ -11,24 +13,6 @@ public class GameSave implements Serializable {
 	private int level;
 	//private Items items;
 	private int HP;
-	/*
-	try{  
-		//what type of file to save as?
-		//FileOutputStream saveGame = new FileOutputStream("SaveGame.");
-
-		//create an ObjectOutputStream to put objects into save file.
-		/*ObjectOutputStream save = new ObjectOutputStream(saveGame);
-
-		save.writeObject(level);
-		//save.writeObject(items);
-		save.writeObject(HP);
-	
-		// Close the file.
-		save.close();
-		}
-		catch(Exception exc){
-			//exc.printStackTrace(); // If there was an error, print the info.
-		}*/
 	
 }
 
