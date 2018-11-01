c0e087b32fd184a91c32d52adf2156021145267d
Peter Wesley Hutcheson <hutcheson8@gmail.com>
2018-10-31 19:24:56 -0500

    Transform ImageGraphics and Load Into Map
    Also, I turned on the background.

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,4 +1,3 @@
-//TODO Figure out why there's blank space around the background. positioning, maybe?
 package game;
 
 import java.awt.Color;
@@ -84,6 +83,7 @@ public class Main implements KeyListener {
 		GAME_WINDOW.setResizable(false);
 		GAME_WINDOW.setLocationRelativeTo(null);
 		GAME_WINDOW.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		currentScene().act();
 		GAME_WINDOW.setVisible(true);
 		long lastFrameTime = System.currentTimeMillis();
 		while (true) {
@@ -121,10 +121,11 @@ public class Main implements KeyListener {
 			}
 		};
 		level.addGraphic(healthbarGraphic);
-		Terrain dirt1 = new Terrain(level.getCameraLocation(), 0, 100.0 / 60, 100.0 / 60, 100.0 / 60, "assets/block.png",
-				8, 1);
+		Terrain dirt1 = new Terrain(level.getCameraLocation(), 0, 100.0 / 60, 100.0 / 60, 100.0 / 60,
+				"assets/block.png", 8, 1);
 		level.addGameObject(dirt1);
-		Obstacle obs1 = new Obstacle(level.getCameraLocation(), 800.0 / 60, 100.0 / 60, 100.0 / 60, 100.0 / 60,"assets/spikes.png", 1, 1, 20);
+		Obstacle obs1 = new Obstacle(level.getCameraLocation(), 800.0 / 60, 100.0 / 60, 100.0 / 60, 100.0 / 60,
+				"assets/spikes.png", 1, 1, 20);
 		level.addGameObject(obs1);
 		Terrain dirt3 = new Terrain(level.getCameraLocation(), 900.0 / 60, 100.0 / 60, 100.0 / 60, 100.0 / 60,
 				"assets/block.png", 2, 1);
@@ -132,60 +133,63 @@ public class Main implements KeyListener {
 		Terrain dirt4 = new Terrain(level.getCameraLocation(), 1100.0 / 60, 0.0 / 60, 100.0 / 60, 100.0 / 60,
 				"assets/block.png", 1, 1);
 		level.addGameObject(dirt4);
-		Terrain dirt5 = new Terrain(level.getCameraLocation(), 1100.0 / 60, 100.0 / 60, 100.0 / 60, 100.0 / 60, "assets/blockBottom.png",
-				1,1);
+		Terrain dirt5 = new Terrain(level.getCameraLocation(), 1100.0 / 60, 100.0 / 60, 100.0 / 60, 100.0 / 60,
+				"assets/blockBottom.png", 1, 1);
 		level.addGameObject(dirt5);
-		Terrain dirt6 = new Terrain(level.getCameraLocation(), 1200.0 / 60, 100.0 / 60, 100.0 / 60, 100.0 / 60, "assets/block.png",
-				1,1);
+		Terrain dirt6 = new Terrain(level.getCameraLocation(), 1200.0 / 60, 100.0 / 60, 100.0 / 60, 100.0 / 60,
+				"assets/block.png", 1, 1);
 		level.addGameObject(dirt6);
-		Obstacle obs2 = new Obstacle(level.getCameraLocation(), 1300.0/60, 100.0/60, 100.0/60, 100.0/60, "assets/spikes.png", 1, 1, 20);
+		Obstacle obs2 = new Obstacle(level.getCameraLocation(), 1300.0 / 60, 100.0 / 60, 100.0 / 60, 100.0 / 60,
+				"assets/spikes.png", 1, 1, 20);
 		level.addGameObject(obs2);
-		Terrain dirt7 = new Terrain(level.getCameraLocation(), 1300.0 / 60, -120.0 / 60, 100.0 / 60, 100.0 / 60, "assets/block.png",
-				1,1);
+		Terrain dirt7 = new Terrain(level.getCameraLocation(), 1300.0 / 60, -120.0 / 60, 100.0 / 60, 100.0 / 60,
+				"assets/block.png", 1, 1);
 		level.addGameObject(dirt7);
-		Terrain dirt8 = new Terrain(level.getCameraLocation(), 1400.0 / 60, 100.0 / 60, 100.0 / 60, 100.0 / 60, "assets/block.png",
-				1,1);
+		Terrain dirt8 = new Terrain(level.getCameraLocation(), 1400.0 / 60, 100.0 / 60, 100.0 / 60, 100.0 / 60,
+				"assets/block.png", 1, 1);
 		level.addGameObject(dirt8);
-		Terrain dirt9 = new Terrain(level.getCameraLocation(), 1500.0 / 60, 100.0 / 60, 100.0 / 60, 100.0 / 60, "assets/blockBottom.png",
-				1,1);
+		Terrain dirt9 = new Terrain(level.getCameraLocation(), 1500.0 / 60, 100.0 / 60, 100.0 / 60, 100.0 / 60,
+				"assets/blockBottom.png", 1, 1);
 		level.addGameObject(dirt9);
-		Terrain dirt10 = new Terrain(level.getCameraLocation(), 1500.0 / 60, 0.0 / 60, 100.0 / 60, 100.0 / 60, "assets/block.png",
-				1,1);
+		Terrain dirt10 = new Terrain(level.getCameraLocation(), 1500.0 / 60, 0.0 / 60, 100.0 / 60, 100.0 / 60,
+				"assets/block.png", 1, 1);
 		level.addGameObject(dirt10);
-		Terrain dirt11 = new Terrain(level.getCameraLocation(), 1600.0 / 60, 0.0 / 60, 100.0 / 60, 100.0 / 60, "assets/blockBottom.png",
-				1,2);
+		Terrain dirt11 = new Terrain(level.getCameraLocation(), 1600.0 / 60, 0.0 / 60, 100.0 / 60, 100.0 / 60,
+				"assets/blockBottom.png", 1, 2);
 		level.addGameObject(dirt11);
-		Terrain dirt12 = new Terrain(level.getCameraLocation(), 1600.0 / 60, -100.0 / 60, 100.0 / 60, 100.0 / 60, "assets/block.png",
-				1,1);
+		Terrain dirt12 = new Terrain(level.getCameraLocation(), 1600.0 / 60, -100.0 / 60, 100.0 / 60, 100.0 / 60,
+				"assets/block.png", 1, 1);
 		level.addGameObject(dirt12);
-		Terrain dirt13 = new Terrain(level.getCameraLocation(), 1700.0 / 60, -100.0 / 60, 100.0 / 60, 100.0 / 60, "assets/blockBottom.png",
-				1,3);
+		Terrain dirt13 = new Terrain(level.getCameraLocation(), 1700.0 / 60, -100.0 / 60, 100.0 / 60, 100.0 / 60,
+				"assets/blockBottom.png", 1, 3);
 		level.addGameObject(dirt13);
-		Terrain dirt14 = new Terrain(level.getCameraLocation(), 1700.0 / 60, -200.0 / 60, 100.0 / 60, 100.0 / 60, "assets/block.png",
-				1,1);
+		Terrain dirt14 = new Terrain(level.getCameraLocation(), 1700.0 / 60, -200.0 / 60, 100.0 / 60, 100.0 / 60,
+				"assets/block.png", 1, 1);
 		level.addGameObject(dirt14);
-		Obstacle obs3 = new Obstacle(level.getCameraLocation(), 1800.0/60, -100.0/60, 100.0/60, 100.0/60, "assets/spikes.png", 1, 1, 20);
+		Obstacle obs3 = new Obstacle(level.getCameraLocation(), 1800.0 / 60, -100.0 / 60, 100.0 / 60, 100.0 / 60,
+				"assets/spikes.png", 1, 1, 20);
 		level.addGameObject(obs3);
-		Obstacle obs4 = new Obstacle(level.getCameraLocation(), 1900.0/60, -100.0/60, 100.0/60, 100.0/60, "assets/spikes.png", 1, 1, 20);
+		Obstacle obs4 = new Obstacle(level.getCameraLocation(), 1900.0 / 60, -100.0 / 60, 100.0 / 60, 100.0 / 60,
+				"assets/spikes.png", 1, 1, 20);
 		level.addGameObject(obs4);
-		Obstacle obs5 = new Obstacle(level.getCameraLocation(), 2000.0/60, -100.0/60, 100.0/60, 100.0/60, "assets/spikes.png", 1, 1, 20);
+		Obstacle obs5 = new Obstacle(level.getCameraLocation(), 2000.0 / 60, -100.0 / 60, 100.0 / 60, 100.0 / 60,
+				"assets/spikes.png", 1, 1, 20);
 		level.addGameObject(obs5);
-		Obstacle obs6 = new Obstacle(level.getCameraLocation(), 2100.0/60, -100.0/60, 100.0/60, 100.0/60, "assets/spikes.png", 1, 1, 20);
+		Obstacle obs6 = new Obstacle(level.getCameraLocation(), 2100.0 / 60, -100.0 / 60, 100.0 / 60, 100.0 / 60,
+				"assets/spikes.png", 1, 1, 20);
 		level.addGameObject(obs6);
-		Terrain dirt15 = new Terrain(level.getCameraLocation(), 1800.0 / 60, 0.0 / 60, 100.0 / 60, 100.0 / 60, "assets/blockBottom.png",
-				4,4);
+		Terrain dirt15 = new Terrain(level.getCameraLocation(), 1800.0 / 60, 0.0 / 60, 100.0 / 60, 100.0 / 60,
+				"assets/blockBottom.png", 4, 4);
 		level.addGameObject(dirt15);
-		Terrain dirt16 = new Terrain(level.getCameraLocation(), 1920.0 / 60, -300.0 / 60, 100.0 / 60, 100.0 / 60, "assets/block.png",
-				4,1);
+		Terrain dirt16 = new Terrain(level.getCameraLocation(), 1920.0 / 60, -300.0 / 60, 100.0 / 60, 100.0 / 60,
+				"assets/block.png", 4, 1);
 		level.addGameObject(dirt16);
-		Terrain portal = new Terrain(level.getCameraLocation(), 2200.0 / 60, -400.0 / 60, 100.0 / 60, 100.0 / 60, "assets/portal.png",
-				1,1);
+		Terrain portal = new Terrain(level.getCameraLocation(), 2200.0 / 60, -400.0 / 60, 100.0 / 60, 100.0 / 60,
+				"assets/portal.png", 1, 1);
 		level.addGameObject(portal);
-		
-		
-		
-		//Graphic background = new ImageGraphic("assets/space.png",0,0,16,9);
-		//level.addGraphic(background);
+
+		Graphic background = new ImageGraphic("assets/space.png", 0, 0, 16, 9, false);
+		level.addGraphic(background);
 		// End level construction.
 
 		// Start of Title construction
