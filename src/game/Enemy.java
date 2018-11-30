ac4233513ca6177af312894e5dfd2562ede557ca
lauramrice <42754734+lauramrice@users.noreply.github.com>
2018-11-29 18:55:23 -0600

    changes

--- src/game/Enemy.java
+++ src/game/Enemy.java
@@ -5,10 +5,11 @@ import java.awt.Point;
 public class Enemy extends Character {
 
 	private static final double WIDTH = 50.0 / 60, HEIGHT = 100.0 / 60;
+	private static final double MIN_X = 5.0 / 60.0;
 	private ImageGraphic enemy = new ImageGraphic("assets/still/player.png", 0, 0, WIDTH, HEIGHT);
 
 	public Enemy(Point cameraLocation) throws Exception {
-		super(cameraLocation, 0, 0, WIDTH, HEIGHT, true);
+		super(cameraLocation, 10, 0, WIDTH, HEIGHT, true);
 		addGraphic(enemy);
 		setHP(50);
 	}
@@ -16,13 +17,10 @@ public class Enemy extends Character {
 	@Override
 	public void updateVelocity() {
 		if (isGrounded()) {
-			setDx(2.0 / 60.0);
+			setDx(-2.0 / 60.0);
+			if (absoluteLocation.getX() <= MIN_X) {
+				setDx(0.0 / 60.0);
+			} 
 		}
-		if (getDx() > 0) {
-			enemy.faceRight();
-		} else if (getDx() < 0) {
-			enemy.faceLeft();
-		}
-
 	}
 }
